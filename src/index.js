53770e1095e668c564069c11b30b268ae6f44226
MarilenaPaoliello <Marilenapaoliello.mp@gmail.com>
2022-06-07 11:52:52 +0200

    Add Board with buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
+    render() {
+        return (
+        <button className="square">
+            {this.props.value}
+        </button>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+    renderSquare(i) {
+      return <Square value={i} />;
+    }
+    render(){
+        const status = "Next player: "; 
+        return(
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>            </div>
+        )
+    }
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Board />);
