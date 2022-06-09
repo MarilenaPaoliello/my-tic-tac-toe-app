482e061f15d0d0f7d4c6b9ddc20efddbd55552a3
MarilenaPaoliello <Marilenapaoliello.mp@gmail.com>
2022-06-09 16:58:17 +0200

    Add flip for second player

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-//creating buttons (square) to pass to the Board
-class Square extends React.Component {
+//A)creating buttons (square) to pass to the Board
+// class Square extends React.Component {
     //B deleting the constructor because Square no longer keeps track of the game’s state
     //A) setting this.state in the constructor to define the status
     /* constructor(props){
@@ -14,11 +14,11 @@ class Square extends React.Component {
             value: null,
         };
     } */
-    render() {
-        return (
+/*     render() {
+        return ( */
             // passing a function as the onClick prop
             //displaing the current state’s value when clicked
-        <button 
+/*         <button 
             className="square" 
             onClick={()=>this.props.onClick({value:"X"})}
         >
@@ -27,7 +27,15 @@ class Square extends React.Component {
         );
     }
 }
-
+ */
+//C) replacing Square class with function component 
+function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
 class Board extends React.Component {
     //adding a constructor to set the state 
     //to the board(parent)to pass to the children
@@ -35,14 +43,20 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            //adding a boolena to set the first move to be “X” by default
+            xIsNext: true,
         };
 
     }
     //adding a method to store into the Board the square status change
     manageClick(i){
         const squares = this.state.squares.slice();
-        squares[i]= "X";
-        this.setState({squares:squares});
+        //adding condition with xIsNext to flip from X to O
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          squares: squares,
+          xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
         //A) passing a prop called value to the Square
@@ -56,7 +70,8 @@ class Board extends React.Component {
       />;
     }
     render(){
-        const status = "Next player: "; 
+        //updating status to displays which player has the next turn
+        const status = "Next player is: " + (this.state.xIsNext ? 'X' : 'O'); 
         return(
             <div>
                 <div className="status">{status}</div>
